4b4e8e801735a5983980d4c9bc92ff81be62309d
Dormiveglia <61294972+Dormiveglia0@users.noreply.github.com>
2026-05-27 19:12:46 +0800

    Generate exec and file eBPF collectors

--- internal/collector/gen.go
+++ internal/collector/gen.go
@@ -1,3 +1,4 @@
 package collector
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -go-package collector -output-dir . Execve ../../bpf/execve.bpf.c -- -I../../bpf -O2 -g
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target bpfel -go-package collector -output-dir . File ../../bpf/file.bpf.c -- -I../../bpf -O2 -g
